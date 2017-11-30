3d15b6ab3127c73d63a6421d0a13c77c48a4d6f5
Mcat12 <newtoncat12@yahoo.com>
2017-11-29 21:13:49 -0500

    Remove wrapper Backtracker struct
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/backtracker.rs
+++ src/backtracker.rs
@@ -4,23 +4,19 @@ pub trait Config: Sized {
     fn is_goal(&self) -> bool;
 }
 
-pub struct Backtracker {}
+pub fn solve<C: Config>(config: C) -> Option<C> {
+    if config.is_goal() {
+        return Some(config);
+    }
 
-impl Backtracker {
-    pub fn solve<C: Config>(config: C) -> Option<C> {
-        if config.is_goal() {
-            return Some(config);
-        }
-
-        for child in config.successors() {
-            if child.is_valid() {
-                match Backtracker::solve(child) {
-                    Some(solution) => return Some(solution),
-                    None => continue
-                }
+    for child in config.successors() {
+        if child.is_valid() {
+            match solve(child) {
+                Some(solution) => return Some(solution),
+                None => continue
             }
         }
-
-        None
     }
+
+    None
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ mod backtracker;
 mod queen;
 
 use queen::QueensConfig;
-use backtracker::Backtracker;
 
 fn main() {
     let queen_config = QueensConfig::new(4, 4);
-    println!("{}", match Backtracker::solve(queen_config) {
+    println!("{}", match backtracker::solve(queen_config) {
         Some(solution) => format!("{}", solution),
         None => "No solution found".to_string()
     });
