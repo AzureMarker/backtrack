9f7b62411599e918baf4ff572db3fcbae9ab6a00
Mcat12 <newtoncat12@yahoo.com>
2017-12-06 00:12:41 -0500

    Setup Trunks test in main
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 mod backtracker;
 mod queen;
+mod trunks;
 
-use queen::QueensConfig;
+use trunks::{Trunk, Suitcase};
 
 fn main() {
-    // Create a starting config for the 8 Queens Puzzle
-    let queen_config = QueensConfig::new(4, 4);
+    // Create a starting config for the Trunk problem
+    let config = Trunk::new(3, 3, &[
+        Suitcase { width: 1, height: 3, name: 'A' },
+        Suitcase { width: 2, height: 1, name: 'B' },
+        Suitcase { width: 1, height: 2, name: 'C' },
+        Suitcase { width: 1, height: 1, name: 'D' },
+        Suitcase { width: 1, height: 1, name: 'E' }
+    ]);
 
     // Solve the puzzle
     println!(
         "{}",
-        match backtracker::solve(queen_config) {
+        match backtracker::solve(config) {
             Some(solution) => format!("{}", solution),
             None => "No solution found".to_string()
         }
