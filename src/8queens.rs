0858265894a1ea4245f4d94fbbffd8b02beb87be
Mcat12 <newtoncat12@yahoo.com>
2017-12-06 22:19:23 -0500

    Restructure to allow for multiple binaries
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/8queens.rs
+++ src/8queens.rs
@@ -1,16 +1,15 @@
-mod backtracker;
-mod queen;
+extern crate backtrack;
 
-use queen::QueensConfig;
+use backtrack::queen::QueensConfig;
 
-fn main() {
+pub fn main() {
     // Create a starting config for the 8 Queens Puzzle
     let queen_config = QueensConfig::new(4, 4);
 
     // Solve the puzzle
     println!(
         "{}",
-        match backtracker::solve(queen_config) {
+        match backtrack::solve(queen_config) {
             Some(solution) => format!("{}", solution),
             None => "No solution found".to_string()
         }
