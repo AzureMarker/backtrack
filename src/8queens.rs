5a20e7a219cfd26c8d09f8f8727c25c7cb17ef75
Mcat12 <newtoncat12@yahoo.com>
2017-12-07 17:38:22 -0500

    Add command line parsing to 8queen as specified in the README
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/8queens.rs
+++ src/8queens.rs
@@ -1,15 +1,35 @@
 extern crate backtrack;
 
+use std::env;
 use backtrack::queen::QueensConfig;
 
 pub fn main() {
-    // Create a starting config for the 8 Queens Puzzle
-    let queen_config = QueensConfig::new(4, 4);
+    // Get the starting config
+    let (row, col) = if env::args().len() == 3 {
+        let args: Vec<usize> = env::args()
+            .skip(1)
+            .take(2)
+            .map(|arg| arg.parse().unwrap_or(0))
+            .collect();
+
+        if args[0] >= 8 || args[1] >= 8 {
+            eprintln!("Location is out of bounds");
+            return;
+        }
+
+        (args[0], args[1])
+    } else {
+        (0, 0)
+    };
+
+    let config = QueensConfig::new(row, col);
+
+    println!("Solving for the first Queen at ({}, {})", row, col);
 
     // Solve the puzzle
     println!(
         "{}",
-        match backtrack::solve(queen_config) {
+        match backtrack::solve(config) {
             Some(solution) => format!("{}", solution),
             None => "No solution found".to_string()
         }
